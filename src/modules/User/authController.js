63f2932a1605af2ea65440444ff5693efd16e856
bogutskii <sl9site@gmail.com>
2024-06-11 12:17:21 -0700

    add ENV

--- src/modules/User/authController.js
+++ src/modules/User/authController.js
@@ -7,7 +7,7 @@ export const register = async (req, res) => {
 
   try {
     // Decrypt the password
-    const bytes = CryptoJS.AES.decrypt(password, 'secret_key');
+    const bytes = CryptoJS.AES.decrypt(password, process.env.SECRET_KEY);
     const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
     // Check if username or email already exists
@@ -16,14 +16,12 @@ export const register = async (req, res) => {
       return res.status(400).json({ error: 'Username or Email already exists' });
     }
 
-    // Create a new user
-    const newUser = new User({ username, password: decryptedPassword, email });
+    // Hash the decrypted password
+    const hashedPassword = await bcrypt.hash(decryptedPassword, 10);
+    const newUser = new User({ username, password: hashedPassword, email });
     await newUser.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    if (error.code === 11000) {
-      return res.status(400).json({ error: 'Username or Email already exists' });
-    }
     res.status(500).json({ error: 'Error registering user' });
   }
 };
@@ -33,19 +31,19 @@ export const login = async (req, res) => {
 
   try {
     // Decrypt the password
-    const bytes = CryptoJS.AES.decrypt(password, 'secret_key');
+    const bytes = CryptoJS.AES.decrypt(password, process.env.SECRET_KEY);
     const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
     // Find the user by username
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(400).json({ error: 'Invalid username or password' });
+      return res.status(400).json({ error: 'Invalid username' });
     }
 
     // Check if the password is correct
     const isMatch = await bcrypt.compare(decryptedPassword, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid username or password' });
+      return res.status(400).json({ error: 'Invalid password' });
     }
 
     res.status(200).json({ message: 'Login successful' });
--- src/modules/User/authRoutes.js
+++ src/modules/User/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { register, login, deleteAllUsers } from './authController.js';
+import { verifyToken, isAdmin } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.delete('/delete-all-users', deleteAllUsers);
+router.delete('/delete-all-users', verifyToken, isAdmin, deleteAllUsers);
 
 export default router;

7bc4969a9ba411de844fb2dde347bc596000d538
bogutskii <sl9site@gmail.com>
2024-06-12 10:11:37 -0700

    fix authorization

--- src/modules/User/authController.js
+++ src/modules/User/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import User from './userModel.js';
 import CryptoJS from 'crypto-js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
   const { username, password, email } = req.body;
@@ -9,16 +10,23 @@ export const register = async (req, res) => {
     const bytes = CryptoJS.AES.decrypt(password, process.env.JWT_SECRET);
     const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
+    console.log('Decrypted Password:', decryptedPassword);
+
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existingUser) {
       return res.status(400).json({ error: 'Username or Email already exists' });
     }
 
     const hashedPassword = await bcrypt.hash(decryptedPassword, 10);
+    console.log('Hashed Password:', hashedPassword);
+
     const newUser = new User({ username, password: hashedPassword, email });
     await newUser.save();
+    console.log('User saved:', newUser);
+
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
+    console.error('Error registering user:', error);
     res.status(500).json({ error: 'Error registering user' });
   }
 };
@@ -27,18 +35,29 @@ export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({ username });
-    if (!user) return res.status(404).json({ error: 'User not found' });
-
     const bytes = CryptoJS.AES.decrypt(password, process.env.JWT_SECRET);
     const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    const isPasswordValid = await bcrypt.compare(decryptedPassword, user.password);
-    if (!isPasswordValid) return res.status(401).json({ error: 'Invalid password' });
+    console.log('Decrypted Password:', decryptedPassword);
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '4h' });
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    console.log('User found:', user);
+
+    const isPasswordValid = await bcrypt.compare(decryptedPassword, user.password);
+    console.log('Password Valid:', isPasswordValid);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.status(200).json({ token });
   } catch (error) {
+    console.error('Error logging in:', error);
     res.status(500).json({ error: 'Error logging in' });
   }
 };

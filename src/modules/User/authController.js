994c9333d76dff76c0acc6b916988483050cc34f
bogutskii <sl9site@gmail.com>
2024-06-11 22:15:00 -0700

    fix register

--- src/modules/User/authController.js
+++ src/modules/User/authController.js
@@ -6,17 +6,14 @@ export const register = async (req, res) => {
   const { username, password, email } = req.body;
 
   try {
-    // Decrypt the password
-    const bytes = CryptoJS.AES.decrypt(password, process.env.SECRET_KEY);
+    const bytes = CryptoJS.AES.decrypt(password, process.env.JWT_SECRET);
     const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    // Check if username or email already exists
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existingUser) {
       return res.status(400).json({ error: 'Username or Email already exists' });
     }
 
-    // Hash the decrypted password
     const hashedPassword = await bcrypt.hash(decryptedPassword, 10);
     const newUser = new User({ username, password: hashedPassword, email });
     await newUser.save();
@@ -30,23 +27,17 @@ export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Decrypt the password
-    const bytes = CryptoJS.AES.decrypt(password, process.env.SECRET_KEY);
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const bytes = CryptoJS.AES.decrypt(password, process.env.JWT_SECRET);
     const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    // Find the user by username
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).json({ error: 'Invalid username' });
-    }
+    const isPasswordValid = await bcrypt.compare(decryptedPassword, user.password);
+    if (!isPasswordValid) return res.status(401).json({ error: 'Invalid password' });
 
-    // Check if the password is correct
-    const isMatch = await bcrypt.compare(decryptedPassword, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid password' });
-    }
-
-    res.status(200).json({ message: 'Login successful' });
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '4h' });
+    res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error: 'Error logging in' });
   }
--- src/modules/middleware/authMiddleware.js
+++ src/modules/middleware/authMiddleware.js
@@ -5,8 +5,7 @@ export const verifyToken = (req, res, next) => {
   if (!token) return res.status(401).json({ error: 'Access denied' });
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (error) {
     res.status(400).json({ error: 'Invalid token' });
